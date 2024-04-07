cfb55f74aee944815d777fdcc1654f4d3540e52f
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-04-07 18:05:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// Constants
+const companyName = 'Scuber';
+
+// Variables
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Exporting variables for testing
+module.exports = {
+    companyName,
+    mostProfitableNeighborhood,
+    companyCeo
+};
